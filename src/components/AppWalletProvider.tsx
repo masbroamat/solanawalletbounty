7cc54e185b8b5c418fd7336dfa513d7a97979735
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2024-12-31 14:44:16 +0800

    Fix build errors for vercel deployment

--- src/components/AppWalletProvider.tsx
+++ src/components/AppWalletProvider.tsx
@@ -9,8 +9,6 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
-
-// imports here
  
 export default function AppWalletProvider({
     children,
@@ -18,11 +16,8 @@ export default function AppWalletProvider({
     children: React.ReactNode;
   }) {
     const network = WalletAdapterNetwork.Devnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), []);
-    const wallets = useMemo(
-      () => [],
-      [],
-    );
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [], []); 
    
     return (
       <ConnectionProvider endpoint={endpoint}>
